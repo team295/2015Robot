978e195d206295555b620319ad22c52f87409cdf
Alex Chew <alex@alexchew.net>
2015-03-07 08:48:55 -0800

    Update successful autonomous sequence with 254: TESTED AND SUCCSSFUL

--- src/com/spcrobotics/commands/AutoWith254.java
+++ src/com/spcrobotics/commands/AutoWith254.java
@@ -3,6 +3,7 @@ package com.spcrobotics.commands;
 import com.spcrobotics.Robot;
 import com.spcrobotics.RobotMap;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
@@ -19,8 +20,10 @@ public class AutoWith254 extends CommandGroup {
 
 	final double CLAW_CLOSE_TIME = 0.4;
 	final double LIFT_TIME       = 0.6;
-	final double MOVE_TIME       = 3.0;
-	final double TURN_TIME       = 0.5;
+	final double TURN1_TIME      = 0.5;
+	final double MOVE1_TIME      = 3.0;
+	final double TURN2_TIME      = 0.9;
+	final double MOVE2_TIME      = 1.5;
 	
 	public AutoWith254() {
 		// Close claw (grip game piece)
@@ -32,40 +35,35 @@ public class AutoWith254 extends CommandGroup {
 			protected void execute() {RobotMap.LIFT_MOTOR.set(LIFT_MOTOR_UP_SPEED);}
 			protected void end()     {RobotMap.LIFT_MOTOR.set(0.0);}
 		});
-		
-		// Move backwards
-		addSequential(new SimpleTimedCommand(MOVE_TIME) {
-			protected void execute() {
-				Robot.drivetrain.setLeft(-1 * DRIVE_LEFT_FORWARD_SPEED);
-				Robot.drivetrain.setRight(-1 * DRIVE_RIGHT_FORWARD_SPEED);
-			}
-			
-			protected void end() {Robot.drivetrain.stop();}
-		});
 
-		// Turn right in place
-		addSequential(new SimpleTimedCommand(TURN_TIME) {
+		addSequential(timedTurnRight(TURN1_TIME));
+		addSequential(timedMoveBack(MOVE1_TIME));
+		addSequential(timedTurnRight(TURN2_TIME));
+		addSequential(timedMoveBack(MOVE2_TIME));
+	}
+	
+	// Move backwards
+	public Command timedTurnRight(double time) {
+		return new SimpleTimedCommand(time) {
 			protected void execute() {
 				Robot.drivetrain.setLeft(DRIVE_LEFT_FORWARD_SPEED * DRIVE_TURN_MULT);
 				Robot.drivetrain.setRight(DRIVE_RIGHT_FORWARD_SPEED * DRIVE_TURN_MULT * -1);
 			}
 			
 			protected void end() {Robot.drivetrain.stop();}
-		});
-		
-//		// Lower lift
-//		addSequential(new SimpleTimedCommand(LIFT_TIME) {
-//			// Only continue going down if we don't hit bottom
-//			@Override
-//			protected void execute() {
-//				if (Robot.lift.isAtBottom()) {end();}
-//				else {RobotMap.LIFT_MOTOR.set(-1 * LIFT_MOTOR_UP_SPEED);}
-//			}
-//			
-//			@Override
-//			protected void end() {RobotMap.LIFT_MOTOR.set(0.0);}
-//		});
-//		
-//		addSequential(new ClawFullOpen());
+		};
 	}
+	
+		// Move backwards
+	public Command timedMoveBack(double time) {
+		return new SimpleTimedCommand(time) {
+			protected void execute() {
+				Robot.drivetrain.setLeft(-1 * DRIVE_LEFT_FORWARD_SPEED);
+				Robot.drivetrain.setRight(-1 * DRIVE_RIGHT_FORWARD_SPEED);
+			}
+			
+			protected void end() {Robot.drivetrain.stop();}
+		};
+	}
+	
 }
